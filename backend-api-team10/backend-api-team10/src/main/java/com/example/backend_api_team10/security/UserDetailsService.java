db823eacaf87c12fcbf2670023447b9c4a594292
Jasmine Butts <64556605+Jasmine-Butts@users.noreply.github.com>
2026-04-22 12:42:25 -0400

    setting up user details service for authentication
    Implement UserDetailsService for loading user details by email.

--- backend-api-team10/backend-api-team10/src/main/java/com/example/backend_api_team10/security/UserDetailsService.java
+++ backend-api-team10/backend-api-team10/src/main/java/com/example/backend_api_team10/security/UserDetailsService.java
@@ -1,1 +1,29 @@
+package com.example.backend_api_team10.security;
 
+import com.example.backend_api_team10.entity.User;
+import com.example.backend_api_team10.repository.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserDetailsService {
+
+  @Autowired
+  private UserRepo userRepo;
+
+  public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+      
+    User user = userRepo.findByEmail(email).orElse(() -> new UsernameNotFoundException("User not found"));
+
+    return new User (
+      user.getEmail(),
+      user.getPasswordHash(),
+      List.of(new SimpleGrantedAuthority("ROLE_" + user.getRole().name()))
+    );
+  }
+  
+}
